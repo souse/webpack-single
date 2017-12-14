d2315bbedf77f8385d9b4776b56fb7fe02588a3b
zhangping <zhangping@shbaoyuantech.com>
2017-12-14 14:14:58 +0800

    add

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Test
+ */
+export default class Test {
+	add(m, n) {
+		return m * n;
+	}
+}
+
+export const multi = (a, b) => {
+	return a + b;
+}
+
+export const dll = (a, b) => {
+	return a - b;
+}
